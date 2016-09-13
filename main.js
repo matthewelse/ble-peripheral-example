a48d0bc0403ff0fea78b4980653cdbc391a980d2
Matthew Else <matthewelse1997@gmail.com>
2016-09-13 15:36:17 +0100

    Change variables to reflect HRM service

--- main.js
+++ main.js
@@ -1,48 +1,46 @@
 var ble = require('ble-peripheral');
 
-var batterySrvcUUID = '180d';
-var batteryCharUUID = '2a37';
-var batteryChar = new ble.Characteristic({
-    uuid: batteryCharUUID,
+var hrmSrvcUUID = '180d'; // Heart Rate Service UUID
+var hrmCharUUID = '2a37'; // Heart Rate Characteristic UUID
+
+var hrmChar = new ble.Characteristic({
+    uuid: hrmCharUUID,
     properties: ['read', 'notify'],
     value: null
 });
-var batteryService = new ble.PrimaryService({
-    uuid: batterySrvcUUID,
-    characteristics: [batteryChar]
+var hrmService = new ble.PrimaryService({
+    uuid: hrmSrvcUUID,
+    characteristics: [hrmChar]
 });
-var batteryLevel = 100;
 
-print('created variables');
+var heart_rate = 100;
 
 ble.onConnection(function() {
-    print("someone connected!!!");
+    print("someone connected!");
 });
 
 ble.onDisconnection(function() {
-    print("disconnected");
+    print("disconnected!");
     ble.startAdvertising();
 });
 
 ble.ready(function() {
     print("ble stack ready");
     ble.setServices([
-        batteryService
+        hrmService
     ]);
     print("added services");
-    ble.startAdvertising("Heart Rate Device", [batterySrvcUUID]);
+    ble.startAdvertising("Heart Rate Device", [hrmSrvcUUID]);
     print("advertising");
 });
 
 setInterval(function() {
     if (ble.isConnected()) {
-        batteryChar.write([batteryLevel]);
+        hrmChar.write([heart_rate]);
 
-        batteryLevel--;
-        if (batteryLevel <= 0) {
-            batteryLevel = 100;
+        heart_rate--;
+        if (heart_rate <= 50) {
+            herat_rate = 100;
         }
     }
 }, 1000);
-
-print("main.js has finished executing.");
